5c51e080ddaeadb293e86e173db04e29eb418bdc
Bhavana-G-Dongre <bhavanagdongre30@gmail.com>
2017-05-16 23:18:18 +0530

    Add files via upload

---
+++ 8a.js
@@ -0,0 +1,32 @@
+var express=require('express');
+var app=express();
+var requestTime=function(req,res,next) {
+	var dt = new Date();
+	var utcDate = dt.toUTCString();
+	req.requestTime=utcDate;
+	next();
+}
+app.use(requestTime);
+app.get('/',function(req,res) {
+	res.send('<h1>Welcome</h1>');
+	console.log("TimeStamp is "+req.requestTime);
+	var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+	console.log("URL is "+fullUrl);
+})
+
+app.get('/8aIndex.html',function(req,res) {
+	console.log("TimeStamp is "+req.requestTime);
+	var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+	console.log("URL is "+fullUrl);
+	res.sendFile(__dirname+"/"+"8aIndex.html");
+})
+
+app.get('/view',function(req,res)
+{
+console.log("TimeStamp is "+req.requestTime);
+var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+console.log("URL is "+fullUrl);
+res.send("Inside View");
+})
+
+app.listen(5000);
